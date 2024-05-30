135076319610a183e78e74b82c57455733674e34
erive <eriversbusiness@gmail.com>
2024-05-30 00:23:44 -0400

    created c# classes for anniversary and address

---
+++ AnniversaryMinder/Anniversary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnniversaryMinder
+{
+    public class Address
+    {
+        public string? StreetAddress { get; set; }
+        public string? Municipality { get; set; }
+        public string? Province { get; set; } // see schema for accepted provinces
+        public string? PostalCode { get; set; }
+    }
+
+    public class Anniversary
+    {
+        [Required]
+        public string Names { get; set; } = ""; 
+        [Required]
+        public DateOnly AnniversaryDate { get; set; } 
+        [Required]
+        public string AnniversaryType { get; set; } = ""; 
+        public string? Description { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public Address? Address { get; set; }
+    }
+
+}
--- AnniversaryMinder/Program.cs
+++ AnniversaryMinder/Program.cs
@@ -1,3 +1,14 @@
 ﻿using Newtonsoft.Json;          // JsonConvert class
 using Newtonsoft.Json.Schema;   // JSchema class 
 using Newtonsoft.Json.Linq;     // JObject class
+
+namespace AnniversaryMinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
