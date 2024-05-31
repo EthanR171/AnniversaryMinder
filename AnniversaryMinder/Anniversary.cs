ca75160f2fb491c8cb017ac55b23f4d8d0a1fd89
erive <eriversbusiness@gmail.com>
2024-05-30 20:38:51 -0400

    m

--- AnniversaryMinder/Anniversary.cs
+++ AnniversaryMinder/Anniversary.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -9,23 +10,40 @@ namespace AnniversaryMinder
 {
     public class Address
     {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string? StreetAddress { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string? Municipality { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string? Province { get; set; } // see schema for accepted provinces
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string? PostalCode { get; set; }
     }
 
     public class Anniversary
     {
         [Required]
-        public string Names { get; set; } = ""; 
+        public string Names { get; set; } = "";
+
         [Required]
-        public DateOnly AnniversaryDate { get; set; } 
+        public DateOnly AnniversaryDate { get; set; }
+
         [Required]
-        public string AnniversaryType { get; set; } = ""; 
+        public string AnniversaryType { get; set; } = "";
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string? Description { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string? Email { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string? Phone { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public Address? Address { get; set; }
     }
 
